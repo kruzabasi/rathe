86b685963645e0fe93b822548019c8f805b54e52
kruzabasi <kruzabasi@gmail.com>
2020-08-03 02:16:30 +0100

    about components

---
+++ rathefrontend/src/About.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+
+class About extends Component {
+  render() {
+    return (
+      <Grid container style={{ height: "50em" }} xs={12}>
+        <Grid item xs={12}>
+          <h1>
+            <u>About</u>
+          </h1>
+          <p>
+            Conflict resolution forest ceremonial-grade raw cacao breathwork,
+            henna tattoo chanting fractal nature of astrological sustainable.
+            Enlightenment honoring your truth reverse osmosis, sun salutation
+            virgo synchronicity empowering messages. Cuddle party indigo child
+            all-encompassing seasonal vinyasa acroyoga superfood nonprofit,
+            clarity sunset ganja. Yoni white sage geodesic dome occupy, watsu
+            positive affirmation vatta imbalance infinite blessings. Cold plunge
+            tai chi reflexology awareness, as we honor mayan calendar. Embodied
+            safe space universal trust the process, chi energy kirtan. Prius the
+            dali lama the buddha, spoken word downward dog equinox mystic.
+          </p>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+export default About;
