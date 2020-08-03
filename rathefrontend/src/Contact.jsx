9216877ab38c66b22426b4ef219e4f91f6754476
kruzabasi <kruzabasi@gmail.com>
2020-08-03 02:16:54 +0100

    contact components

---
+++ rathefrontend/src/Contact.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div style={{ height: "60em" }}>
+        <h1>Contact Us</h1>
+      </div>
+    );
+  }
+}
